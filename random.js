614445969b93d279c2c6ccb3e69740a59b788d48
yellowboletus <s.zuercher@websan.ch>
2020-04-12 12:36:30 +0200

    1 Speichertest an Ostern auf public

---
+++ random.js
@@ -0,0 +1,6 @@
+// 09-03 random (video 09-03b)
+
+for (i = 0; i < 5; i += 1){
+    var x = Math.floor(Math.random() * 51) //random number 1 - 50, add +1!!!
+    console.log(x)
+}
---
+++ random.py
@@ -0,0 +1,17 @@
+# 09-02 Random number -> falsch in Terminal
+# import random
+
+# number = random.randint(1, 51)
+# print(number)
+
+# number = random.randint(1, 51)
+# print(number)
+
+# number = random.randint(1, 51)
+# print(number)
+
+# video -> ebenfalls falsch im Terminal!
+import random
+
+for i in range(5):
+    print(random.randint(1, 51)) # I want 1 - 51 (false in Video!)
